acedb89e9a13319324b08e3f2f4f0e3ffb7bf3d7
Amkeez252 <auwalamalik@gmail.com>
2023-05-01 00:24:29 -0700

    fixing bugs

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Sep  1 14:42:23 2020
-@authors: Robinson Montes
-          Mauricio Olarte
+Created by Auwal Abdulmalik on 
+        A Colaboration project
+
 """
 from flask import Blueprint, jsonify, request, abort
 from api.v1.views import app_views
